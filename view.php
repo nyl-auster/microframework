94a12fce5258d37eb86c05a47ab5988335ece17f
yann <yann.boisselier@gmail.com>
2013-08-07 19:09:17 +0200

    fix comments

--- view.php
+++ view.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Render a templates. Support wrapper templates.
+ * Template engine.
  */
 class view {
 
@@ -8,6 +8,12 @@ class view {
   public $file = '';
   private $wrapperView = null;
 
+  /**
+   * @param string $file
+   *   full relative path to the template file.
+   * @param array $variables
+   *   associatives array of variables to pass to the template file.
+   */
   public function __construct($file, $variables = array()) {
     $this->file = $file;
     $this->variables = $variables;
@@ -17,6 +23,8 @@ class view {
    * Display a template, inside a wrapper template if asked.
    * A variable $innerView is created and must be put inside wrapper template
    * to display the innerTemplate.
+   * @return string
+   *   content of the file, once php variables have been parsed
    */
   public function render() {
     $output = $this->includeParse($this->file, $this->variables); 
@@ -27,6 +35,10 @@ class view {
     return $output;
   }
 
+  /**
+   * Include a file and parse php variablews without printing it.
+   * @return string
+   */
   public function includeParse($file, $variables = array()) {
     if ($variables) extract($variables);
     ob_start();
