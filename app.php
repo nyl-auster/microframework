c7cb1ed42c87d3fad75bbc59e754fb7f39bdedf8
yann <yann.boisselier@gmail.com>
2013-08-07 12:26:46 +0200

    remove comment from app.php

--- app.php
+++ app.php
@@ -2,54 +2,7 @@
 /**
  * Tiny class to map a route to a controller method.
  *
- * HOW TO USE
- * ------------
- *
- * 1 - Call this code in the index.php at the root of your application.
- * @code
- * require 'app.php';
- * $app = new app();
- * print $app->executeRoute($app->getPath());
- * @endcode
- *
- * 2 - Create routes in a ini file, in "conf" directory at the root of your application.
- * @code
- * [hello/world]
- * path = controllers
- * class = myController
- * method = helloWorld
- * @encode
- *
- * - hello/world mean than your controller will respond going on "domain.com/index.php/hello/world" url.
- * - class is your class name to instanciate. Your file have to be name myControler.php in this example.
- * - method : method that will be called for this url.
- *
- * 3 - Create your controller in "controllers/myController.php" file, extending defaultController :
- * @code
- * class myController extends defaultController {
- * 
- *   function helloWorld() {
- *     return 'Hello world';
- *     // you may use a template this way :
- *     // return $this->view('templates/helloWorld.html.php');
- *   }
- *
- * }
- * @endcode
- *
- * 4 - goto to yourdomain.com/index.php/hello/world and see "hello world" displaying.
- *
- * DIRECTORIES STRUCTURE
- * --------------------
- *
- * app.php
- * conf/
- * libraries/
- * vendor/
- * templates/
- * controllers/
- *
- * Do not write "index.php" in hard in this file, we may use other entry point if wanted.
+ * @TODO  * Do not write "index.php" in hard in this file, we may use other entry point if wanted.
  */
 class app {
 
