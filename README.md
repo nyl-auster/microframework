997cba1f5e21dfdd9b86badc0e18c5d026e21914
nyl_auster <yann.boisselier@gmail.com>
2013-08-08 23:10:23 +0200

    update routes

---
+++ README.md
@@ -0,0 +1,6 @@
+Php MiniFramework
+------------------
+
+Php mini-framework with simple controller and view classes. Only map an incoming http request to a class method, using a routes.ini file to do so. Rename examples.routes.ini to routes.ini to create custom routes.
+
+
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-require 'modules/microframework/core/controller.php';
-require 'modules/microframework/core/view.php';
+require 'modules/miniFramework/core/controller.php';
+require 'modules/miniFramework/core/view.php';
 $routes = is_readable('routes.ini') ? parse_ini_file('routes.ini', true) : array();
 $controller = new controller($routes);
 print $controller->executePath($controller->getRequestedPath());
