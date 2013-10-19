49a263b03f0486aa79093cdb2d12038c256cf155
nyl_auster <yann.boisselier@gmail.com>
2013-10-19 17:14:13 +0200

    fix registry file

---
+++ example.registry.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Ressource registry example. Rename to registry.php to use.
+ * 
+ * $registry['exampleRessource'] = array(
+ *   'class' => 'microframework\core\ressources\exampleRessource',
+ *   'route' => 'example',
+ * );
+ *
+ * include 'microframework/mybundle/mymodule/registry.php
+ * 
+ */
+
