cd20aef450d8b86da7f73b7cbb24072e2d5eb4da
nyl_auster <yann.boisselier@gmail.com>
2013-10-20 12:53:50 +0200

    document example.registry.php

--- example.registry.php
+++ example.registry.php
@@ -1,13 +1,23 @@
 <?php
 /**
+ * @file
  * Ressource registry example. Rename to registry.php to use.
- * 
- * $registry['exampleRessource'] = array(
- *   'class' => 'microframework\core\ressources\exampleRessource',
- *   'route' => 'example',
- * );
- *
- * include 'microframework/mybundle/mymodule/registry.php
- * 
  */
+  
+// register a new ressource, responding to "example" url.
+$registry['example'] = array(
+  'class' => 'myBundle\myModule\exampleRessource',
+  'route' => 'example',
+);
+
+// override the default ressources provided by server classe :
+// custom homepage
+$registry['homepage']['class'] = 'myBundle\myModule\myRessource';
+// custom 404 ressource
+$registry['http404']['class'] = 'myBundle\myModule\myRessource';
+// custom 403 ressource.
+$registry['http403']['class'] = 'myBundle\myModule\myRessource';
+
+// includes may be used to let each module have its own registry file.
+include 'microframework/mybundle/mymodule/registry.php
 
