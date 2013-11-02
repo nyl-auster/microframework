c2bc9fced3daa23e8d5c02ffb59703584daa3e05
nyl_auster <yann.boisselier@gmail.com>
2013-11-02 15:02:04 +0100

    add comments

--- modules/microframework/database/database.php
+++ modules/microframework/database/database.php
@@ -3,6 +3,9 @@ namespace microframework\database;
 
 class database {
 
+  /**
+   * callback for app.bootstrap event
+   */
   function connect() {
     if (isset($settings['mysql'])) {
       $mysqlLink = mysql_connect($settings['mysql']['server'], $settings['mysql']['user'], $settings['mysql']['password'])
@@ -11,6 +14,9 @@ class database {
     }
   }
 
+  /**
+   * callback for app.close event
+   */
   function close() {
     if (isset($mysqlLink)) {
       mysql_close($mysqlLink);
