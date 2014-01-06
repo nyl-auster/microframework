157057d71d5eb9678779c256de32d9700103052c
Yann <yann.boisselier@gmail.com>
2014-01-06 19:29:43 +0100

    remove example package

--- packages/okc/demo/config/translations.php
+++ packages/okc/demo/config/translations.php
@@ -3,8 +3,15 @@
 $_translations['en_EN']['light_view_controller_php_framework'] = 'View-controller PHP Framework';
 $_translations['en_EN']['french'] = 'French';
 $_translations['en_EN']['english'] = 'English';
+$_translations['en_EN']['hello_world'] = 'Hello World';
+$_translations['en_EN']['hello_world_test_page'] = 'Hello world test page';
+$_translations['en_EN']['page_layout'] = 'Layout template';
+
 
 $_translations['fr_FR']['light_view_controller_php_framework'] = 'Framework PHP View-Controller';
 $_translations['fr_FR']['french'] = 'Français';
 $_translations['fr_FR']['english'] = 'Anglais';
+$_translations['fr_FR']['hello_world'] = 'Bonjour Monde';
+$_translations['fr_FR']['hello_world_test_page'] = 'Page de test';
+$_translations['fr_FR']['page_layout'] = 'Template de mise en page';
 
