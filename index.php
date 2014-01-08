c9eb469bbe56f939acc003b24cf8c7f9b9914a0b
Yann <yann.boisselier@gmail.com>
2014-01-08 20:07:07 +0100

    Add dependency injection

--- index.php
+++ index.php
@@ -1,30 +1,55 @@
 <?php
-use okc\server\server;
-use okc\eventsManager\eventsManager;
-
-$containerSettings = array(
- 'packagesManager' => array(
-   'class' => 'okc\packageManager\packageManager',
-   'contruct' => array(
-     'packages' => 'packages',
-     'config' => 'config',
-   ),
-);
-
-$container = new container();
-$container->getService('packageManager'
+use okc\container\container;
 
 // set autoloader PSR-0 
 set_include_path(implode(PATH_SEPARATOR, array(get_include_path(), 'packages', 'vendors')));
 spl_autoload_register(function($class){include_once preg_replace('#\\\|_(?!.+\\\)#','/',$class).'.php';}); 
 
-// now app can fire events calling fire static method.
-eventsManager::fire('frameworkBootstrap');
+$diParams = array(
+  'i18n' => array(
+    'default' => array(
+      'class' => 'okc\i18n\i18n',
+    ),
+  ),
 
-// fetch resource matching current url
-$server = new server();
+  'packagesManager' => array(
+
+    'default' => array(
+      'class' => 'okc\packagesManager\packagesManager',
+      'arguments' => array(
+        'packages' => array('type' => 'int'),
+        'config' => array('type' => 'string'),
+      ),
+    )
+  ),
+  'server' => array(
+    'default' => array(
+      'class' => 'okc\server\server',
+    ),
+  ),
+
+  'resource' => array(
+    'default' => array(
+      'class' => 'okc\resource\resource',
+    ),
+  ),
+
+);
+
+$di = new container($diParams);
+$pm = $di->get('packagesManager');
+
+$server = $di->get('server');
 $resource = $server->getResource($server->getRouteFromUrl());
 print $resource->render();
 
-eventsManager::fire('frameworkShutdown');
+// now app can fire events calling fire static method.
+// eventsManager::fire('frameworkBootstrap');
+
+// fetch resource matching current url
+// $server = new server();
+// $resource = $server->getResource($server->getRouteFromUrl());
+// print $resource->render();
+
+// eventsManager::fire('frameworkShutdown');
 
--- packages/okc/container/container.php
+++ packages/okc/container/container.php
@@ -1,5 +1,5 @@
 <?php
-namespace okc\container\container;
+namespace okc\container;
 
 class container {
 
@@ -9,8 +9,66 @@ class container {
     $this->config = $config;
   }
 
-  function getService($className) {
+  /**
+   * Get a service. 
+   * @param string $name
+   *   the service name
+   * @param string $context
+   *   different set of params may be set.
+   *   if $context is an array; this will be used directly as arguments
+   *   to the class constructor.
+   */
+  function get($name, $context = 'default') {
+    $arguments = array();
+    if (is_array($context)) {
+      $arguments = $context;
+      $class = new \ReflectionClass($this->config[$name]['default']['class']);
+      $object = $class->newInstanceArgs($arguments);
+      return $object;
+    }
+    else {
+      $datas = $this->config[$name][$context];
+      if (!empty($datas['arguments'])) {
+        $arguments = $this->parseServiceArguments($datas['arguments']);
+      }
+      $class = new \ReflectionClass($datas['class']);
+      $object = $class->newInstanceArgs($arguments);
+      return $object;
+    }
+  }
 
+  /**
+   * @param array $arguments
+   *   associative array containing :
+   *   -context
+   *     - class : class with its namespace
+   *     - arguments : array containing
+   *       - Argument name => type of argument
+   *
+   * Example :
+   *
+   * array(
+   *   'default' => array(
+   *     'class' => 'okc\okc\server',
+   *     'arguments' => array(
+   *       'routes' => 'array',
+   *       'config' => 'service',
+   *     ),
+   *   ),
+   * );
+   *
+   */
+  function parseServiceArguments($arguments) {
+    $args = array();
+    foreach ($arguments as $value => $description) {
+      if ($description['type'] == 'service') {
+        $args[] = $this->get($value);
+      }
+      else {
+        $args[] = $value;
+      }
+    }
+    return $args;
   }
 
 }
