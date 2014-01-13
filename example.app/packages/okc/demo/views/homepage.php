a1d857df6523dce06a08874454dca927c7625d99
Yann <yann.boisselier@gmail.com>
2014-01-13 13:58:41 +0100

    fix demo homepage

--- example.app/packages/okc/demo/views/homepage.php
+++ example.app/packages/okc/demo/views/homepage.php
@@ -2,8 +2,8 @@
 use okc\server\server;
 ?>
 
-<p>This is a demo homepage, provided by <em>packages/okc/demo</em> package. <br/>
-<p> Copy example.app/ to app/ to start coding. </p>
+<p>This is a demo homepage, provided by <em>app/packages/okc/demo</em> package. <br/>
+<p> This is an example module to start coding. </p>
 
 <p> Check also <?php echo server::link('Hello World', 'hello-world')?> test page. </p>
 
