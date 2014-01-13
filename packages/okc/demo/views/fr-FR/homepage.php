3b9f63f048e6adb4cba5208bef7561214b7078b2
Yann <yann.boisselier@gmail.com>
2014-01-13 11:56:11 +0100

    remove unused firectory from demo package

--- packages/okc/demo/views/fr-FR/homepage.php
+++
@@ -1,7 +0,0 @@
-<?php
-use okc\server\server;
-$this->setParentView('okc/demo/views/layout.php');
-?>
-
-<p>Ceci est la page d'accueil de démonstration, fournie par le package <em> packages/okc/demo </em>. <br/></p>
-
