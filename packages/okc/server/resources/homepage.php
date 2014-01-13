361800607015bd4955814e9c61f6cc3a5c026f60
Yann <yann.boisselier@gmail.com>
2014-01-13 13:44:03 +0100

    change installation process

--- packages/okc/server/resources/homepage.php
+++ packages/okc/server/resources/homepage.php
@@ -6,7 +6,18 @@ use okc\resource\resource;
 class homepage extends resource {
 
   function get() {
-    return 'Default homepage.';
+    $out = '<style>';
+    $out .= '*{padding : 0; margin : 0;}';
+    $out .= 'body {color : #DDD; background-color : #111; text-align : center; padding-top : 50px; font-size : 35px;}';
+    $out .= 'strong {color : orange;}';
+    $out .= '</style>';
+    $out .= '<html>';
+    $out .= '<body>';
+    $out .= 'Hello ! <br/><br/>';
+    $out .= 'Please rename <strong>example.app</strong> directory to <strong>app</strong> to install and start coding.';
+    $out .= '</body>';
+    $out .= '</html>';
+    return $out;
   }
 
 }
